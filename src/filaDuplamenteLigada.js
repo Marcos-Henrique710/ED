d0c48b597d5d397dcc0ac7e46fad2ceb79f13924
Marcos-Henrique710 <marcoshenriqueufrn@gmail.com>
2026-03-18 16:45:10 -0300

    implementando as demais funções da fila duplamente ligada, como remoção de elementos e busca.

--- src/filaDuplamenteLigada.js
+++ src/filaDuplamenteLigada.js
@@ -1,15 +1,18 @@
 import noDuplo from "./noDuplo";
 
+//Implementação de uma fila duplamente ligada
 class filaDuplamenteLigada {
     constructor(dado) {
         this.head = null;
         this.tail = null;
     }
 
+    //Verificar se a lista está vazia
     isEmpty() {
         this.head === null;
     }
 
+    //Adicionar um elemento no início da lista
     add(dados) {
         let novoNo = new noDuplo();
         if(this.isEmpty()){
@@ -22,6 +25,7 @@ class filaDuplamenteLigada {
         }
     }
 
+    //Adicionar um elemento no final da lista
     append(dados) {
         let novoNo = new noDuplo(dado);
 
@@ -35,10 +39,29 @@ class filaDuplamenteLigada {
         }
     }
 
+    //Adicionar um elemento em uma posição específica da lista
     addAt(dado, pos) {
+        let novoNo = new noDuplo(dado);
 
+        if(pos === 0) {
+            this.add(dado);
+        }else {
+            let aux = this.head;
+
+            for(let i = 0; i < pos - 1; i++) {
+                if(aux.proximo === null) {
+                    throw new Error("Posição inválida");
+                }
+                aux = aux.proximo;
+            }
+            novoNo.proximo = aux.proximo;
+            novoNo.anterior = aux;
+            aux.proximo.anterior = novoNo;
+            aux.proximo = novoNo;
+        }
     }
 
+    //Remover o primeiro elemento da lista
     removeFist() {
         if(this.isEmpty()){
             throw new Error ("Lista vazia");
@@ -51,6 +74,7 @@ class filaDuplamenteLigada {
         }
     }
 
+    //Remover o último elemento da lista
     removeLast() {
         if(this.isEmpty()){
             throw new Error("Lista vazia")
@@ -63,11 +87,36 @@ class filaDuplamenteLigada {
         }
     }
 
-    revoveAt(pos) {
-
+    //Remover um elemento em uma posição específica da lista
+    removeAt(pos) {
+        if(this.isEmpty()){
+            throw new Error("Lista vazia");
+        }else {
+            let aux = this.head;
+            for(let i = 0; i < pos - 1; i++) {
+                if(aux.proximo === null) {
+                    throw new Error("Posição inválida");
+                }
+                aux = aux.proximo;
+            }
+            aux.proximo = aux.proximo.proximo;
+            aux.proximo.anterior = aux;
+        }
     }
 
+    //Buscar um elemento na lista
     search() {
-
+        if(this.isEmpty()) {
+            throw new Error("Lista vazia");
+        }else {
+            let aux = this.head;
+            while(aux !== null) {
+                if(aux.dado === dado) {
+                    return true;
+                }
+                aux = aux.proximo;
+            }
+            return false;
+        }
     }
 }
